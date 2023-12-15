590d4edb6e55ad813c4a7eff7bd2feb030b15182
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2023-12-15 20:30:04 +0000

    first

---
+++ 01_first.js
@@ -0,0 +1,25 @@
+const score = 400
+
+const balance = new Number(100.6789599)
+// console.log(balance);
+
+// console.log(balance.toFixed(1))
+
+const otherNumber = 123.8967
+
+// console.log(otherNumber.toPrecision(4))
+
+const hundreds = 100000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// +++++++++++++++++++++++++ maths
+
+// console.log(Math.abs(4));
+
+// console.log(Math.round(4.2))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.2))
+// console.log(Math.max(2,3,4,5,6,7,89))
+console.log((Math.random()*10)+1)
