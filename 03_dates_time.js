5da2fe0fc3441b8a0f434f51d97281dad3e62a93
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2023-12-15 20:39:18 +0000

    date and time

---
+++ 03_dates_time.js
@@ -0,0 +1,26 @@
+let myDate = new Date()
+
+// console.log(myDate.toString())
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toTimeString());
+
+// console.log(typeof myDate)
+
+let myCreatedDate = new Date("01-14-2023")
+
+// console.log(myCreatedDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.round(Date.now()/1000));
+
+let newDate = new Date()
+
+console.log(newDate.getDay());
+
+
