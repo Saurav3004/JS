9f96559a77e8970204bb6dfbcda41735b272c2d5
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2023-12-16 11:02:02 +0000

    for loop

---
+++ 09_iteration.js
@@ -0,0 +1,24 @@
+for(let i=0;i<=10;i++){
+    const element = i;
+    if (element === 5) {
+        // console.log("Access Granted");
+        
+    }else{
+    function check(num1){
+        let result = num1 + 2
+        return result;
+    }
+    // console.log(check(element))
+    
+    }
+    
+
+}
+// console.log(element);
+
+for(i=1;i<=10;i++){
+    console.log((`table of ${i}`));
+    for(j=1;j<=10;j++){
+        console.log(`${i} * ${j} = ${i*j}`);
+    }
+}
