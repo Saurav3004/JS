c9d674ff553a3a1ad72df1cf59cedc7e54bb656d
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2023-12-16 11:38:55 +0000

    do while

---
+++ 10_do_while.js
@@ -0,0 +1,21 @@
+let i = 0
+// i=2
+while(i<=10){
+    // console.log(`${i}`);
+    i+=2
+}
+
+let myArray = ['saurav','shubham','maths','science']
+ let index = 0;
+
+
+while(index<=myArray.length-1){
+    //  console.log(myArray[index]);
+     index+=1
+}
+
+let score = 1;
+do{
+console.log(`score is ${score}`);
+score++
+}while(score<=10)
