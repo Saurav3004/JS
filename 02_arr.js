8d4c0955bbb7d903b8d535c92efdaddd445c733d
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2023-12-17 09:03:04 +0000

    array

--- 02_arr.js
+++ 02_arr.js
@@ -14,12 +14,14 @@ const myArr2 = new Array(1,2,3,4,5)
 // console.log(typeof newArr);
 console.log("A" , myArr)
 let arrN1 = myArr.slice(1,3)
-
 console.log(arrN1)
-console.log("B" , myArr)
 
+
+console.log("B" , myArr)
 let arrN2 = myArr.splice(1,3)
 console.log(arrN2)
+
+
 console.log("C" , myArr)
 
 // splice manipulates the original array whereas slice shows only its indexes'value and doesn't change in the original array!!!
