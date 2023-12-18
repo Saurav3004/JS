af641d8b0b433ea822b0a945a14d37bd7818dcc6
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2023-12-18 02:37:51 +0000

    function with object

--- 05_funcobj.js
+++ 05_funcobj.js
@@ -1,17 +1,30 @@
-function calculateCartprice(...num1){
-    return num1
+function calculateCartprice(...num1) {
+    return (num1)
 }
-calculateCartprice(2)
+console.log(calculateCartprice(100,200,400,500,700,800,9996,798));
+const totalPriceArray = (calculateCartprice(100, 200, 400, 500, 700, 800, 9996, 798));
+// const finalListPrice = totalPriceArray.map((items,index,array)=>{
+//     if(array[index]>800){
+//          return array[index] += 4
+//     }else{
+//         return array[index];
+//     }
+//  }).reduce((accu,currentVal)=>{
+//     return accu + currentVal
+//  },0)
+//  console.log(finalListPrice);
+ 
 
-// console.log(calculateCartprice(2,3,4));
+
+
 
 const user = {
     username: "saurav",
     "Book Price": 199
 }
 
-function handleObject(anyObject){
-        // console.log(`username is ${anyObject.usernames} and the book price is ${anyObject["Book Price"]}`);
+function handleObject(anyObject) {
+    // console.log(`username is ${anyObject.username} and the book price is ${anyObject["Book Price"]}`);
 }
 // handleObject(user)
 
@@ -21,15 +34,15 @@ handleObject({
 
 })
 
-const myNewArray = [200,400,100,600]
-const myNewArray2 = [1,2,3,4]
+const myNewArray = [200, 400, 100, 600]
+const myNewArray2 = [1, 2, 3, 4]
 
-function returnSecondValue(getArray){
+function returnSecondValue(getArray) {
     return getArray[1]
 }
 const a = returnSecondValue(myNewArray)
 const b = returnSecondValue(myNewArray2)
-// console.log(a, ",",b); 
+console.log(a, ",",b); 
 
 //in arguements we pass the array in which we apply our method or in which we will find our results and further it goes to parameteric value and in function we return our paramteric value with their specific syntax....
 
